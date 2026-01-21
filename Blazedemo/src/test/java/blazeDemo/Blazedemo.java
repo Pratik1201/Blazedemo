a39ce790da65e711455d98c78a97dac4cc21c1d8
Pratik1201 <pratikhsonawane@gmail.com>
2026-01-21 20:35:06 +0530

    Added steps

---
+++ Blazedemo/src/test/java/blazeDemo/Blazedemo.java
@@ -0,0 +1,30 @@
+package blazeDemo;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Blazedemo {
+	static WebDriver driver;
+	public static void main(String[] args) {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://blazedemo.com");
+		System.out.println(driver.getTitle());
+		WebElement dropdown1 = driver.findElement(By.xpath("//select[@name='fromPort']"));
+		Select s1 =  new Select(dropdown1);
+		s1.selectByVisibleText("Mexico City"); 
+		WebElement dropdown2 = driver.findElement(By.xpath("//select[@name='toPort']"));
+		Select s2 = new Select(dropdown2);
+		s2.selectByVisibleText("New York");
+		driver.findElement(By.cssSelector(".btn.btn-primary")).click();
+		
+		
+	}
+
+}
